473f0fdb70acb3eff5a74c71a22745c1fbf0bf69
5h4k4r <shakar.halabjai@yahoo.com>
2022-06-22 23:18:08 +0300

    Add icons into icons module

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { FeatherModule } from 'angular-feather';
-import { ArrowRightCircle, Menu } from 'angular-feather/icons';
+import { ArrowRightCircle, Menu, Send, Phone } from 'angular-feather/icons';
 
 // Select some icons (use an object, not an array)
 const icons = {
-  ArrowRightCircle
-  ,
-  Menu
+  ArrowRightCircle,
+  Menu,
+  Send,
+  Phone
 };
 
 @NgModule({

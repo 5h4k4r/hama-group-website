0c2ca445b3d3103e5eaa89a3e1c77aa441070987
5h4k4r <shakar.halabjai@yahoo.com>
2022-06-22 18:53:56 +0300

    update package json:    angular-feather     animate.css     aos     bootstrap     gsap     jquery     ng2-animate-on-scroll

---
+++ src/app/components/test/test.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    const nodes = document.getElementsByClassName('node');
+    const container = document.getElementById('box2');
+
+    if (!container || !nodes)
+      return;
+
+    window.onscroll = () => {
+
+      const scrollView = window.scrollY / container.clientHeight / 4 - 0.75;
+
+      if (scrollView < 0.25) {
+        this.clearNodes()
+        nodes[0].classList.add('bolden-text');
+      }
+      else if (scrollView > 0.25 && scrollView < 0.5) {
+        this.clearNodes()
+        nodes[1].classList.add('bolden-text');
+      }
+      else if (scrollView > 0.5 && scrollView < 0.6) {
+        this.clearNodes()
+        nodes[2].classList.add('bolden-text');
+      }
+      else if (scrollView > 0.6) {
+        this.clearNodes()
+        nodes[3].classList.add('bolden-text');
+      }
+
+
+    }
+
+
+  }
+  private clearNodes() {
+    const nodes = document.getElementsByClassName('node');
+
+    if (!nodes)
+      return;
+
+    for (let i = 0; i < nodes.length; i++) {
+      const element = nodes[i];
+      element.classList.remove('bolden-text')
+    }
+  }
+}

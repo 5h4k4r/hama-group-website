f133b5503a3d161fcf3541176b91cdcf5b5f9777
5h4k4r <shakar.halabjai@yahoo.com>
2022-06-23 21:51:24 +0300

    Create component for horizontal cards in big screens

---
+++ src/app/components/horizontal-scroll-cards/horizontal-scroll-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-scroll-cards',
+  templateUrl: './horizontal-scroll-cards.component.html',
+  styleUrls: ['./horizontal-scroll-cards.component.scss']
+})
+export class HorizontalScrollCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

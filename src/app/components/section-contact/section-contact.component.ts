33352f7af3dbcf22ba8e24b6e9991b005923785a
5h4k4r <shakar.halabjai@yahoo.com>
2022-06-24 16:11:51 +0300

    Update contact section

--- src/app/components/section-contact/section-contact.component.ts
+++ src/app/components/section-contact/section-contact.component.ts
@@ -12,4 +12,17 @@ export class SectionContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  clickToCall(): void {
+    window.open('tel:+9647703614849');
+  }
+
+  openMessenger(): void {
+    // https://m.me/119173678135946
+    window.location.href = 'https://m.me/119173678135946';
+  }
+
+  openEmail(): void {
+    window.location.href = "mailto:info@hamafx.com";
+
+  }
 }
